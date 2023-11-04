160d46019d126cf09ba69b5573cf88069f595332
ChefAharoni <ChefAharoni@proton.me>
2023-11-04 12:18:55 -0400

    Initial Commit

---
+++ CUNYCoin/CUNYCoinApp.swift
@@ -0,0 +1,17 @@
+//
+//  CUNYCoinApp.swift
+//  CUNYCoin
+//
+//  Created by Amit Aharoni on 11/4/23.
+//
+
+import SwiftUI
+
+@main
+struct CUNYCoinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
